6d5a3cd0273b836e48ed9687efb94f0635bbf224
moennig1997 <tamura-y@terra.dti.ne.jp>
2019-03-20 00:54:02 +0900

    add logger and

--- securityCamera.py
+++ securityCamera.py
@@ -18,6 +18,9 @@ from email.MIMEMultipart import MIMEMultipart
 from email.utils import formatdate
 from retry import retry
 
+import logging
+
+
 def create_message(from_addr, to_addr,  subject, body, attach):
     msg = MIMEMultipart()
     msg['Subject'] = subject
@@ -45,10 +48,29 @@ def send(from_addr, to_addr, smtp_host, smtp_port, smtp_user, smtp_pass, msg):
     smtpobj = smtplib.SMTP(smtp_host, smtp_port)
     smtpobj.ehlo()
     smtpobj.starttls()
+
     smtpobj.ehlo()
     smtpobj.login(smtp_user, smtp_pass)
     smtpobj.sendmail(from_addr, to_addr, msg.as_string())
     smtpobj.close()
+
+
+#create logger
+logger = logging.getLogger('seccamera_application')
+logger.setLevel(logging.DEBUG)
+# create file handler which logs even debug messages
+fh = logging.FileHandler('securityCamera.log')
+fh.setLevel(logging.DEBUG)
+# create console handler with a higher log level
+ch = logging.StreamHandler()
+ch.setLevel(logging.ERROR)
+# create formatter and add it to the handlers
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+fh.setFormatter(formatter)
+ch.setFormatter(formatter)
+# add the handlers to the logger
+logger.addHandler(fh)
+logger.addHandler(ch)
 
 
 # Read paramater from config file
@@ -66,12 +88,12 @@ smtp_port = int(config.get('mail', 'smtp_port'))
 smtp_user = config.get('mail', 'smtp_user')
 smtp_pass = config.get('mail', 'smtp_pass')
 
-print(from_addr) 
-print(to_addr)
-print(smtp_host)
-print(smtp_port)
-print(smtp_user)
-print(smtp_pass)
+logger.info(from_addr) 
+logger.info(to_addr)
+logger.info(smtp_host)
+logger.info(smtp_port)
+logger.info(smtp_user)
+logger.info(smtp_pass)
 
 
 # image capture
@@ -92,39 +114,53 @@ try:
 		if r == True:
 
 			hash = imagehash.average_hash(Image.fromarray(img))
-			print(hash)
-			print(prev_hash)
+			#logger.info(hash)
+			#logger.info(prev_hash)
 
 			if prev_hash is None or prev_hash - hash > threshold:
-				change_detected = 1
+				change_detected = 3
 				now = datetime.datetime.now(timezone('Asia/Tokyo'))
 				datestr = '{0:%Y%m%d%H%M%S}'.format(now)
-				print('Detect somthing change!!....' + datestr)
+				logger.info('Detect something change!!....' + datestr)
 				cv2.imwrite(datestr + 'detected.jpg', img)
 				detect_images.append(datestr + 'detected.jpg')
-			elif change_detected == 1:
-				attache_image = Image.new('RGB', (640, 480 * len(detect_images)))
-				for i, image in enumerate(detect_images):
-					im = Image.open(image)
-					attache_image.paste(im, (0, 480*i))
-					im.close()
+
+			elif change_detected > 0:
 
 				now = datetime.datetime.now(timezone('Asia/Tokyo'))
 				datestr = '{0:%Y%m%d%H%M%S}'.format(now)
-				attache_image.save('attache_' + datestr + '.jpeg')
-				attache_image.close()
-				detect_images = []
+				logger.info('Detect something change!!....' + datestr)
+				cv2.imwrite(datestr + 'detected.jpg', img)
+				detect_images.append(datestr + 'detected.jpg')
 
-				msg = create_message(from_addr, to_addr, 'Detect somthing change!!....' + datestr,'Detect somthing change!!....' + datestr, 'attache_' + datestr + '.jpeg')
-				send(from_addr, to_addr, smtp_host, smtp_port, smtp_user, smtp_pass, msg )
-				change_detected = 0
-			else:
-				change_detected = 0
+				change_detected -= 1
+				logger.info('counter:'+str(change_detected))
+
+				if change_detected == 0:
+					attache_image = Image.new('RGB', (640, 480 * len(detect_images)))
+					for i, image in enumerate(detect_images):
+						im = Image.open(image)
+						attache_image.paste(im, (0, 480*i))
+						im.close()
+
+					now = datetime.datetime.now(timezone('Asia/Tokyo'))
+					datestr = '{0:%Y%m%d%H%M%S}'.format(now)
+					attache_image.save('attache_' + datestr + '.jpeg')
+					attache_image.close()
+					detect_images = []
+
+					msg = create_message(from_addr, to_addr, 'Detect something change!!....' + datestr,'Detect something change!!....' + datestr, 'attache_' + datestr + '.jpeg')
+					send(from_addr, to_addr, smtp_host, smtp_port, smtp_user, smtp_pass, msg )
+					change_detected = 0
+
+					logger.info('send message')
+
+				
 
 			
 			prev_hash = copy.deepcopy(hash)
 
-		print('next')
+		#logger.info('next')
 
 		if cv2.waitKey(1) & 0xFF == ord('q'):
 			break
